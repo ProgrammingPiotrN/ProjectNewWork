b1cce0394750c95a520371e6aafb56380ab8e762
Piotr Nawrocki <piotr_naw99@outlook.com>
2024-05-01 00:40:50 +0200

    01052024 Dodanie ikon wiadomości i wyszukiwania. Stworzenie szablonu formularza logowania i rejestracji.

--- Blog/resources/views/auth/login.blade.php
+++ Blog/resources/views/auth/login.blade.php
@@ -1,47 +1,49 @@
-<x-guest-layout>
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
+<link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
 
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
+<div class="bg-gray-100 flex h-screen items-center justify-center px-4 sm:px-6 lg:px-8">
+    <div class="w-full max-w-md space-y-8">
+        <div class="bg-white shadow-md rounded-md p-6">
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+            <img class="mx-auto h-10 w-auto" src="{{ asset('assets/img/logo.png') }}" alt="" />
+
+            <h2 class="my-3 text-center text-3xl font-bold tracking-tight text-gray-900">
+                Login
+            </h2>
+
+
+            <form class="space-y-6" method="POST" action="{{ route('login') }}">
+                @csrf
+
+                <div>
+                    <label for="email" class="block text-sm font-medium text-gray-700">Email:</label>
+                    <div class="mt-1">
+                        <input name="email" type="email" autocomplete="email-address"
+                            class="px-2 py-3 mt-1 block w-full rounded-md border border-gray-300 shadow-sm focus:border-sky-500 focus:outline-none focus:ring-sky-500 sm:text-sm" />
+                    </div>
+                    @error('email')
+                        <span class="text-red-500 text-sm">{{ $message }}</span>
+                    @enderror
+                </div>
+
+                <div>
+                    <label for="password" class="block text-sm font-medium text-gray-700">Password:</label>
+                    <div class="mt-1">
+                        <input name="password" type="password" autocomplete="password"
+                            class="px-2 py-3 mt-1 block w-full rounded-md border border-gray-300 shadow-sm focus:border-sky-500 focus:outline-none focus:ring-sky-500 sm:text-sm" />
+                    </div>
+                    @error('password')
+                        <span class="text-red-500 text-sm">{{ $message }}</span>
+                    @enderror
+                </div>
+
+                <div>
+                    <button type="submit"
+                        class="flex w-full justify-center rounded-md border border-transparent bg-blue py-2 px-4 text-sm font-medium text-black shadow-sm hover:bg-opacity-75 focus:outline-none focus:ring-2 focus:ring-blue focus:ring-offset-2">Login
+                        </button>
+                </div>
+            </form>
         </div>
+    </div>
+</div>
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
 
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Remember Me -->
-        <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-            </label>
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
-
-            <x-primary-button class="ms-3">
-                {{ __('Log in') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
--- Blog/resources/views/welcome.blade.php
+++ Blog/resources/views/welcome.blade.php
@@ -26,47 +26,55 @@
         <nav class="p-5 bg-white shadow md:flex md:items-center md:justify-between">
             <div class="flex justify-between items-center">
                 <span class="text-2xl font-[Poppins] cursor-pointer">
-                    <img class="h-10 inline" src="{{ asset('assets/img/logo.png') }}" alt="Logo">
                     Blog IT
+                    <img class="h-16 inline" src="{{ asset('assets/img/logo.png') }}" alt="Logo">
                 </span>
                 <span class="text-3xl cursor-pointer mx-2 md:hidden block">
                     <ion-icon name="menu-outline" onclick="Menu(this)"></ion-icon>
                 </span>
             </div>
 
+            <div class="flex justify-center w-100 items-center">
+                <span class="text-3xl cursor-pointer mx-2   flex justify-between">
+                    <ion-icon name="chatbox-outline" class="text-5xl"></ion-icon>
+                </span>
+                <span class="text-3xl cursor-pointer mx-2   flex justify-between">
+                    <ion-icon name="search-circle-outline" class="text-5xl"></ion-icon>
+                </span>
+            </div>
+
             <ul class="md:flex md:items-center z-[-1] md:z-auto md:static absolute bg-white w-full left-0 md:w-auto
             md:py-0 py-4 md:pl-0 pl-7 md:opacity-100 opacity-0 top-[-400px] transition-all ease-in duration-500">
                 <li class="mx-4 my-6 md:my-0">
-                    <a href="#" class="text-xl hover:text-blue-500 duration-500">MESSAGE</a>
-                </li>
-                <li class="mx-4 my-6 md:my-0">
-                    <a href="#" class="text-xl hover:text-blue-500 duration-500">LOOP</a>
-                </li>
-                <li class="mx-4 my-6 md:my-0">
-                    <a href="#" class="text-xl hover:text-blue-500 duration-500">HOME</a>
-                </li>
-                <li class="mx-4 my-6 md:my-0">
                     <a href="#" class="text-xl hover:text-blue-500 duration-500">CREATE POST</a>
                 </li>
-                <li class="mx-4 my-6 md:my-0 space-x-4">
+
                     @if (Route::has('login'))
                         @auth
                             <a href="{{ url('/dashboard') }}" class="text-xl hover:text-blue-500 duration-500">
                                 Dashboard
                             </a>
                         @else
+                        <li class="mx-4 my-6 md:my-0">
                             <a href="{{ route('login') }}" class="text-xl hover:text-blue-500 duration-500">
                                 Log in
                             </a>
+                        </li>
 
-                               @if (Route::has('register'))
-                                <a  href="{{ route('register') }}" class="text-xl hover:text-blue-500 duration-500">
+
+                            @if (Route::has('register'))
+                                <li class="mx-4 my-6 md:my-0">
+                                    <a  href="{{ route('register') }}" class="text-xl hover:text-blue-500 duration-500">
                                     Register
-                                </a>
-                                @endif
+                                    </a>
+                                </li>
+
+                            @endif
                         @endauth
                     @endif
-                </li>
+
+
+
             </ul>
         </nav>
 
